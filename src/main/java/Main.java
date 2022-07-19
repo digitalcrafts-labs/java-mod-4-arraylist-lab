212050b11b470e85d4e049c0f45bf18159c017dd
Abdella <abdella.shukur2@gmail.com>
2022-07-19 08:30:46 -0400

    merged list and removed negative #s

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,14 @@ public class Main {
     }
 
     public static ArrayList<Integer> mergeTwoArrayLists(ArrayList<Integer> l1, ArrayList<Integer> l2) {
-        // your code here
-        return null;
+        // merge two lists and print positive values 
+        // e.g 4,13,5 - list 1; 7,3,5,10,8 - list 2 
+        // e.g 3,4,5,5,7,8,10,13 - new list 
+        ArrayList<Integer> favNums = new ArrayList<>();
+        favNums.addAll(l1);
+        favNums.addAll(l2);
+        favNums.removeIf(val -> val < 0);
+
+        return favNums;
     }
 }
